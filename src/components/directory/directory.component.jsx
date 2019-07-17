b98bbe5df268b8467e149c3f140ff3a3cc0e2f4f
Marcus Chui <marcuschui@icloud.com>
2019-07-18 01:13:36 +0800

    updating our app with a shop page, collection preview component and collection item component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.component.styles.scss';
-import { sections } from './directory.data';
+import SECTIONS from './directory.data';
 
 export default class Directory extends Component {
   constructor() {
     super();
     this.state = {
-      sections
+      SECTIONS
     };
   }
 
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+        {this.state.SECTIONS.map(({ id, ...otherSectionProps }) => (
           <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
